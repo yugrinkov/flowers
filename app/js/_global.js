7caa65f859497725beae4c68b4cd03a63f9ae8f9
Yurii Grynkov <Yurii.Grynkov@hrs.com>
2021-08-30 18:12:53 +0300

    Added pinterest home world

--- app/js/_global.js
+++ app/js/_global.js
@@ -1,4 +1,7 @@
 // Global
+
+window.app = {};
+
 app.global = {
     init: function(){ // Load all global functions here
         console.log("load global functions");
--- app/js/script.js
+++ app/js/script.js
@@ -1,4 +1,24 @@
 console.log("Load script.js");
 
-// Instantiating the global app object
-var app = {};
+function getMoreButtonPopupElement () {
+    return document.getElementsByClassName('jsMoreButtonPopup')[0];
+}
+
+function getMoreButtonElement () {
+    return document.getElementsByClassName('jsMoreButton')[0];
+}
+
+document.getElementsByClassName('jsMoreButton')[0].addEventListener('click', function (event) {
+    event.stopPropagation();
+    var moreButtonPopupElement = getMoreButtonPopupElement();
+    var moreButtonElement = getMoreButtonElement();
+    moreButtonPopupElement.classList.remove('hide');
+    moreButtonElement.classList.add('active');
+})
+
+document.addEventListener("click", function () {
+    var moreButtonPopupElement = getMoreButtonPopupElement();
+    var moreButtonElement = getMoreButtonElement();
+    moreButtonPopupElement.classList.add('hide');
+    moreButtonElement.classList.remove('active');
+})
